965f11a9b36f8d044695acf8be5abbae4109352b
arara100 <aararaaararv2@gmail.com>
2024-11-08 10:56:42 +0200

    veb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import PopularGames from './components/PopularGames';
+import SpecialOffers from './components/SpecialOffers';
+import Footer from './components/Footer';
+import Catalog from './components/Catalog';
+import ProductDetail from './components/ProductDetail';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Hero />
+      
+      <Routes>
+        <Route path="/" element={<PopularGames />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/special-offers" element={<SpecialOffers />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src="deps/img/logo.png" alt="Nintendo Switch" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Каталог</a></li>
+          <li><a href="#">Акції</a></li>
+          <li><a href="#">Блог</a></li>
+          <li><a href="#">Контакти</a></li>
+        </ul>
+      </nav>
+      <div className="header-right">
+        <a href="#">Увійти</a>
+        <a href="#" className="cart-icon">Кошик</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
